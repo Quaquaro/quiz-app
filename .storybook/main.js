692a077a673bff0330b554a3a03370bdbe5dca43
Christoph Timm <quaquaro@gmail.com>
2022-01-19 16:09:33 +0100

    Install and configure Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  stories: ["../**/*.stories.[tj]s"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/html",
+};
---
+++ styles/components/app-grid.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Components/App-grid",
+};
+
+export const Default = () => `<div class="app-grid">Hello world</div>`;
