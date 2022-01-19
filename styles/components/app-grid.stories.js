c40cc3feb01e6b168048faa9df1c8899e3ee7285
Christoph Timm <quaquaro@gmail.com>
2022-01-19 18:05:35 +0100

    Make the HTML-Files and link them to each other

--- styles/components/app-grid.stories.js
+++ styles/components/app-grid.stories.js
@@ -2,4 +2,12 @@ export default {
   title: "Components/App-grid",
 };
 
-export const Default = () => `<div class="app-grid">Hello world</div>`;
+export const Default = () => `
+<div class="app-grid h-500">
+  <header class="bg-pink">top</header>
+  <main class="bg-blue app-grid__scrollarea">
+    <div class="h-100vh">scroll me</div>
+  </main>
+  <nav class="bg-coral">bottom</nav>
+</div>
+`;
