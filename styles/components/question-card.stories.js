3726f8ac44a1ed34c5149ef72b75c4049372d54f
Christoph Timm <quaquaro@gmail.com>
2022-01-20 17:48:54 +0100

    Add colors to colors.css

--- styles/components/question-card.stories.js
+++ styles/components/question-card.stories.js
@@ -6,7 +6,7 @@ export const Default = () => `
       <section class="question-card rounded border-solid m-4">
         Question:
         <button class="question-card__bookmark border-none p-0">
-          <img src="../../images/bookmark-empty.svg" alt="Bookmark" width="32" height="32" />
+          <img src="../../images/bookmark-empty.svg" alt="Bookmark" width="32" height="32">
         </button>
         <p class="answer">Answer:</p>
         <button class="show">Show Answer</button>
