09f782ebfc970398237873504d3e30d6c789b427
Christoph Timm <quaquaro@gmail.com>
2022-01-24 17:11:20 +0100

    Add some JavaScript

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+//Set Bookmark
+console.clear();
+const bookmarkButton = document.querySelector(".question-card__bookmark");
+const bookmark = document.querySelector(".bookmark");
+bookmarkButton.addEventListener("click", () => {
+  bookmark.src = "./images/bookmark-filled.svg";
+});
+
+//Show Answers
+const answerButton = document.querySelector(".button");
+const answerText = document.querySelector(".answer--hidden");
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("answer--hidden");
+  answerButton.innerText = "Hide answer";
+  answerButton.classList.add("hide");
+});
