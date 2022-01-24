127107df18002a8985cf84a3db10ab295763867c
Christoph Timm <quaquaro@gmail.com>
2022-01-24 10:18:48 +0100

    Add .vscode to .gitignore

--- styles/components/footer.stories.js
+++ styles/components/footer.stories.js
@@ -5,16 +5,16 @@ export default {
 export const Default = () => `
     <nav class="p-2 mx-4 flex justify-evenly flex-wrap footer rounded-b-lg">
       <a href="#" class="footer__link"
-        ><img src="../../images/home.svg" alt="Gehe zur Startseite" height="32" width="32"
-      /></a>
+        ><img src="/../../images/home.svg" alt="Gehe zur Startseite" height="32" width="32"
+      ></a>
       <a href="pages/bookmarks.html" class="footer__link"
-        ><img src="../..images/bookmark-filled.svg" alt="Lesezeichen" width="32" height="32"
-      /></a>
+        ><img src="/../../images/bookmark-filled.svg" alt="Lesezeichen" width="32" height="32"
+      ></a>
       <a href="pages/form.html" class="footer__link"
-        ><img src="../..images/edit.svg" alt="Frage erstellen" height="32" width="32"
-      /></a>
+        ><img src="../../images/edit.svg" alt="Frage erstellen" height="32" width="32"
+      ></a>
       <a href="pages/form.html" class="footer__link">
-        <img src="../..mages/avatar.svg" alt="zur Profilseite" height="32" width="32" />
+        <img src="../../images/avatar.svg" alt="zur Profilseite" height="32" width="32">
       </a>
     </nav>
 `;
