fb65ca30e741f515b84fac8d969db1f469797d07
Christoph Timm <quaquaro@gmail.com>
2022-01-27 11:33:33 +0100

    Add Counter

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,80 @@
+// function for querySelector
+
+function getQuerySelectorJsData(jsDataValue) {
+  return document.querySelector(`[data-js="${jsDataValue}"]`);
+}
+
+//function for querySelectorAll
+
+function getQuerySelectorAllJsData(jsDataValue) {
+  return document.querySelectorAll(`[data-js="${jsDataValue}"]`);
+}
+
 //Set Bookmark
-console.clear();
-const bookmarkButton = document.querySelector(".question-card__bookmark");
-const bookmark = document.querySelector(".bookmark");
-bookmarkButton.addEventListener("click", () => {
-  bookmark.src = "./images/bookmark-filled.svg";
+
+const bookmarks = getQuerySelectorAllJsData('quiz');
+
+bookmarks.forEach((bookmark) => {
+  bookmark.addEventListener('click', () => {
+    
+  });
 });
 
-//Show Answers
-const answerButton = document.querySelector(".button");
-const answerText = document.querySelector(".answer--hidden");
+function addBookmark (){
+    if (bookmark.src.includes('bookmark-empty')) {
+      bookmark.src = './images/bookmark-filled.svg';
+    } else {
+      bookmark.src = './images/bookmark-empty.svg';
+    }
+}
+// const bookmark = getQuerySelectorJsData('bookmark');
 
-answerButton.addEventListener("click", () => {
-  answerText.classList.toggle("answer--hidden");
-  answerButton.innerText = "Hide answer";
-  answerButton.classList.add("hide");
+// bookmark.addEventListener('click', () => {
+
+// });
+
+// const bookmarkButton = document.querySelector(".question-card__bookmark");
+// const bookmark = document.querySelector(".bookmark");
+// bookmarkButton.addEventListener("click", () => {
+//   bookmark.src = "./images/bookmark-filled.svg";
+// });
+
+//Show Answers
+// const answerButton = document.querySelector(".button");
+// const answerText = document.querySelector(".answer--hidden");
+
+// answerButton.addEventListener("click", () => {
+//   answerText.classList.toggle("answer--hidden");
+//   answerButton.innerText = "Hide answer";
+//   answerButton.classList.add("hide");
+// });
+
+//Counter for textarea
+const textareaContentAnswer = document.querySelector('[data-js="add-answer"]');
+const counterElement = document.querySelector('.textcount');
+
+textareaContentAnswer.addEventListener('input', () => {
+  let textlength = textareaContentAnswer.value.length;
+  counterElement.value = textlength + ' of 200';
+  textareaContentAnswer.classList.remove('form__textarea--red-border');
+  if (textlength > 200) {
+    //Farbe ändern mit ClassList Red Counter and Red Border
+    textareaContentAnswer.classList.add('form__textarea--red-border');
+  }
+});
+
+const textareaContentQuestion = document.querySelector(
+  '[data-js="add-question"]'
+);
+const counterQuestion = document.querySelector(
+  '[data-js="add-question-counter"]'
+);
+
+textareaContentQuestion.addEventListener('keyup', () => {
+  let textlength = textareaContentQuestion.value.length;
+  counterQuestion.value = textlength + ' of 200';
+  textareaContentQuestion.classList.remove('form__textarea--red-border');
+  if (textlength > 200) {
+    textareaContentQuestion.classList.add('form__textarea--red-border');
+  }
 });
